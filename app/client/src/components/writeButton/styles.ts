ef50921ae3288eeefbf2968f9ba08ebc09a128d7
bae080311 <s24024@gsm.hs.kr>
2025-02-19 21:01:45 +0900

    Style: button text

--- app/client/src/components/writeButton/styles.ts
+++ app/client/src/components/writeButton/styles.ts
@@ -8,4 +8,10 @@ export const Button = styled.button`
   justify-content: space-between;
   align-items: center;
   border: none;
+  color: #111;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  text-transform: capitalize;
 `;

a2d86ffd7b5bedbfd24e1f0eb8c852e365888983
bae080311 <s24024@gsm.hs.kr>
2025-02-19 18:55:43 +0900

    Fix: routing

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,15 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./app/globalStyle";
 import { Router } from "./app/router";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <GlobalStyle />
-      <Routes>
-        <Route path="/client/*" element={<Router />} />
-      </Routes>
-    </BrowserRouter>
+      <Router />
+    </>
   );
 }
 
--- app/client/src/app/router.tsx
+++ app/client/src/app/router.tsx
@@ -1,12 +1,14 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/login";
 import Main from "../pages/main";
 
 export const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/main" element={<Main />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/main" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
   );
 };

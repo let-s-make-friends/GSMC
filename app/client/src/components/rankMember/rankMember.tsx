2f4e53a96462482c6bce02cd44d21e527a3db897
bae080311 <s24024@gsm.hs.kr>
2025-02-19 20:48:25 +0900

    Feat: rank member component

---
+++ app/client/src/components/rankMember/rankMember.tsx
@@ -0,0 +1,14 @@
+import * as S from "./styles";
+
+const RankMember = () => {
+  return (
+    <S.Wrapper>
+      <div>
+        <S.name>배경진진</S.name>
+      </div>
+      <S.score>2300</S.score>
+    </S.Wrapper>
+  );
+};
+
+export default RankMember;
---
+++ app/client/src/components/rankMember/styles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const name = styled.span`
+  color: #000;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  text-transform: capitalize;
+`;
+
+export const score = styled.span`
+  color: #003cff;
+  font-family: Pretendard;
+  font-size: 0.75rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  text-transform: capitalize;
+  border-radius: 1.5rem;
+  background: #f1f1f1;
+  padding: 0.5rem 1rem;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.75rem 0rem;
+`;

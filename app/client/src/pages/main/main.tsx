91c5dfe09dc6a727bf7ef7c831c93fbcb34e5491
bae080311 <s24024@gsm.hs.kr>
2025-02-19 21:00:23 +0900

    Feat: main

--- app/client/src/pages/main/main.tsx
+++ app/client/src/pages/main/main.tsx
@@ -2,22 +2,26 @@ import Header from "../../components/header/header";
 import WriteButton from "../../components/writeButton/writeButton";
 import PFA from "../../assets/img/PFA.png";
 import * as S from "./styles";
+import Rank from "../../components/rank/rank";
 
 const Main = () => {
   return (
     <S.Container>
       <Header />
       <S.Wrapper>
-        <div>
+        <S.SmallWrapper>
           <div>
             <S.Title>랭킹</S.Title>
+            <Rank />
           </div>
           <div>
             <S.Title>작성</S.Title>
-            <WriteButton label="활동 영역 작성" location="/write" />
-            <WriteButton label="독서 영역 작성" location="/writeBook" />
+            <S.ButtonWrapper>
+              <WriteButton label="활동 영역 작성" location="/write" />
+              <WriteButton label="독서 영역 작성" location="/writeBook" />
+            </S.ButtonWrapper>
           </div>
-        </div>
+        </S.SmallWrapper>
         <img src={PFA} alt="PFA" />
       </S.Wrapper>
     </S.Container>
---
+++ app/client/src/pages/main/styles.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10rem;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  gap: 2rem;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+export const Title = styled.h2`
+  color: #fff;
+  font-family: "LOTTERIA CHAB";
+  font-size: 2rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  text-transform: capitalize;
+  margin-bottom: 1rem;
+`;
+
+export const SmallWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+  flex-direction: column;
+`;

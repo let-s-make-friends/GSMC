e9d3a5e39b44a81fb8a047a1d031cbc1e226fcb8
bae080311 <s24024@gsm.hs.kr>
2025-02-28 22:35:53 +0900

    ♻️ refactor: signup code

--- app/client/src/pages/signup/signup.tsx
+++ app/client/src/pages/signup/signup.tsx
@@ -4,15 +4,12 @@ import Input from "../../components/input/input";
 import DefaultButton from "../../components/defaultButton/defaultButton";
 import { API } from "../../types/api";
 import { useState, useEffect } from "react";
+import { Auth, CertificateCode } from "../../types/auth";
 
-interface CertificateCode {
-  email: string;
-}
-
-interface Auth extends CertificateCode {
-  password: string;
-  authCode: string;
-}
+// sxxxxx@gsm.hs.kr
+const emailRegEx = /^s\d{5}@gsm\.hs\.kr$/;
+// 영문, 숫자, 특수문자를 포함하여 8글자 이상으로 적어주세요.
+const passwordRegEx = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/;
 
 const Signup = () => {
   const [email, setEmail] = useState("");
@@ -23,16 +20,8 @@ const Signup = () => {
   const [signupActive, setSignupActive] = useState(false);
   const [authCodeActive, setAuthCodeActive] = useState(false);
 
-  // sxxxxx@gsm.hs.kr
-  const emailRegEx = /^s\d{5}@gsm\.hs\.kr$/;
-
-  // 영문, 숫자, 특수문자를 포함하여 8글자 이상으로 적어주세요.
-  const passwordRegEx = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/;
-
   const submitSignUp = async () => {
-    if (signupActive == false) {
-      return;
-    }
+    if (signupActive == false) return;
     const res = await API<Auth>("/api/v1/auth/sign-up", {
       method: "POST",
       body: {
@@ -41,6 +30,7 @@ const Signup = () => {
         authCode: authCode,
       },
     });
+    res.success;
   };
 
   const getAuthCode = async () => {
@@ -52,33 +42,20 @@ const Signup = () => {
         email: email,
       },
     });
-  };
-
-  const handleSignup = () => {
-    if (
-      !email ||
-      !password ||
-      !emailRegEx.test(email) ||
-      !passwordRegEx.test(password) ||
-      password !== passwordCheck
-    ) {
-      setSignupActive(false);
-    } else {
-      setSignupActive(true);
-    }
-  };
-
-  const handleAuthCode = () => {
-    if (!email || !emailRegEx.test(email)) {
-      setAuthCodeActive(false);
-    } else {
-      setAuthCodeActive(true);
-    }
+    res.success;
   };
 
   useEffect(() => {
-    handleSignup();
-    handleAuthCode();
+    setSignupActive(
+      !(
+        !email ||
+        !password ||
+        !emailRegEx.test(email) ||
+        !passwordRegEx.test(password) ||
+        password !== passwordCheck
+      )
+    );
+    setAuthCodeActive(!(!email || !emailRegEx.test(email)));
   }, [email, password, passwordCheck]);
 
   return (
@@ -98,7 +75,11 @@ const Signup = () => {
             onClick={getAuthCode}
           />
         </S.EmailWrapper>
-        <Input label="인증번호" placeholder="인증번호 입력란" />
+        <Input
+          label="인증번호"
+          onChange={(e) => setAuthCode(e.target.value)}
+          placeholder="인증번호 입력란"
+        />
         <Input
           label="새 비밀번호"
           placeholder="영문, 숫자 포함 8글자 이상으로 구성"

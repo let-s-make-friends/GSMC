9d684cf38b75fa9a6e532b7a1b086c24596d1947
bae080311 <s24024@gsm.hs.kr>
2025-03-03 04:56:10 +0900

    🏷️  Feat: activity topic type 추가

--- app/client/src/types/activity.ts
+++ app/client/src/types/activity.ts
@@ -12,6 +12,7 @@ export type category = "인문" | "전공";
 
 export interface Activity {
   studyCategory: category; // "인문" | "전공"
+  topic: string; // 주제
   activityCategory: category2; // "교내 수상" | "교외 수상" | "교내 참가" | "교외 참가" | "동아리" | "인성 교외 수상" | "인성 교내 수상" | "인문 활동"
   body: string; // 글 본문
   activityDate: string; // 학기 yyyy-MM-dd

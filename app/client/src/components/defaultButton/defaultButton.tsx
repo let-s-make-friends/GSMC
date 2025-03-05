253a37a82aa03a9da8f164154cd488d516692588
Dino0204 <s24020@gsm.hs.kr>
2025-03-05 17:42:45 +0900

    💄 Update: DefaultButton

--- app/client/src/components/defaultButton/defaultButton.tsx
+++ app/client/src/components/defaultButton/defaultButton.tsx
@@ -3,13 +3,14 @@ import * as S from "./styles";
 interface DefaultButtonProps {
   label: string,
   active: boolean,
+  fullW?: boolean,
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
 
-const DefaultButton = ({ label, active, onClick }: DefaultButtonProps) => {
+const DefaultButton = ({ label, active, fullW = false, onClick }: DefaultButtonProps) => {
   return (
-    <S.Button onClick={onClick} $isActive={active} >
-      <S.Label $isActive={active}>{label}</S.Label>
+    <S.Button onClick={onClick} isActive={active} isFull={fullW} >
+      <S.Label isActive={active}>{label}</S.Label>
     </S.Button >
   );
 };

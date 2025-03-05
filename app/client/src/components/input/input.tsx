d35ea0c5c2bd84dc42b42f43bb0536eead84af65
Dino0204 <s24020@gsm.hs.kr>
2025-03-05 17:42:59 +0900

    💄 Update: Input

--- app/client/src/components/input/input.tsx
+++ app/client/src/components/input/input.tsx
@@ -1,19 +1,15 @@
 import * as S from "./styles";
 
 interface InputProps {
-  label: string,
   type?: "email" | "password" | "text",
   placeholder?: string,
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void,
   onClick?: (e: React.MouseEvent<HTMLInputElement>) => void
 }
 
-const Input = ({ label, type, placeholder = "", onClick, onChange }: InputProps) => {
+const Input = ({ type, placeholder = "", onClick, onChange }: InputProps) => {
   return (
-    <S.InputWrapper>
-      <S.InputLabel>{label}</S.InputLabel>
-      <S.Input type={type} placeholder={placeholder} onClick={onClick} onChange={onChange} />
-    </S.InputWrapper>
+    <S.Input type={type} placeholder={placeholder} onClick={onClick} onChange={onChange} />
   );
 };
 export default Input;

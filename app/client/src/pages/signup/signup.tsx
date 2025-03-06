11cea9e495667180d75cefe8f7fe6750e0d7b430
Dino0204 <s24020@gsm.hs.kr>
2025-03-06 21:00:55 +0900

    💄 Update: Login, Signup Align Center

--- app/client/src/pages/signup/signup.tsx
+++ app/client/src/pages/signup/signup.tsx
@@ -35,60 +35,63 @@ const Signup = () => {
   return (
     <S.Container>
       <Header />
-      <S.Wrapper>
-        <S.SecondWrapper>
-          <S.InputWrapper>
-            <S.InputLabel>이메일</S.InputLabel>
-            <S.EmailWrapper>
+      <S.LoginWrapper>
+
+        <S.Wrapper>
+          <S.SecondWrapper>
+            <S.InputWrapper>
+              <S.InputLabel>이메일</S.InputLabel>
+              <S.EmailWrapper>
+                <Input
+                  placeholder="@gsm.hs.kr"
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <DefaultButton
+                  label="인증번호"
+                  active={authCodeActive}
+                  onClick={async () => {
+                    const res = await getAuthCode(email);
+                    res.success ? setEmail(email) : setEmail("");
+                  }}
+                />
+              </S.EmailWrapper>
+            </S.InputWrapper>
+            <S.InputWrapper>
+              <S.InputLabel>인증번호</S.InputLabel>
               <Input
-                placeholder="@gsm.hs.kr"
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => setAuthCode(e.target.value)}
+                placeholder="인증번호 입력란"
               />
-              <DefaultButton
-                label="인증번호"
-                active={authCodeActive}
-                onClick={async () => {
-                  const res = await getAuthCode(email);
-                  res.success ? setEmail(email) : setEmail("");
-                }}
+            </S.InputWrapper>
+            <S.InputWrapper>
+              <S.InputLabel>새 비밀번호</S.InputLabel>
+              <Input
+                placeholder="영문, 숫자 포함 8글자 이상으로 구성"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
               />
-            </S.EmailWrapper>
-          </S.InputWrapper>
-          <S.InputWrapper>
-            <S.InputLabel>인증번호</S.InputLabel>
-            <Input
-              onChange={(e) => setAuthCode(e.target.value)}
-              placeholder="인증번호 입력란"
-            />
-          </S.InputWrapper>
-          <S.InputWrapper>
-            <S.InputLabel>새 비밀번호</S.InputLabel>
-            <Input
-              placeholder="영문, 숫자 포함 8글자 이상으로 구성"
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </S.InputWrapper>
-          <S.InputWrapper>
-            <S.InputLabel>새 비밀번호 재입력</S.InputLabel>
-            <Input
-              placeholder="재입력란"
-              type="password"
-              onChange={(e) => setPasswordCheck(e.target.value)}
-            />
-          </S.InputWrapper>
-        </S.SecondWrapper>
-        <DefaultButton
-          label="회원가입"
-          fullW={true}
-          active={signupActive}
-          onClick={async () => {
-            const res = await submitSignUp(email, password, Number(authCode));
-            res.success ? go("/login") : setEmail("");
-          }}
-        />
-      </S.Wrapper>
+            </S.InputWrapper>
+            <S.InputWrapper>
+              <S.InputLabel>새 비밀번호 재입력</S.InputLabel>
+              <Input
+                placeholder="재입력란"
+                type="password"
+                onChange={(e) => setPasswordCheck(e.target.value)}
+              />
+            </S.InputWrapper>
+          </S.SecondWrapper>
+          <DefaultButton
+            label="회원가입"
+            fullW={true}
+            active={signupActive}
+            onClick={async () => {
+              const res = await submitSignUp(email, password, Number(authCode));
+              res.success ? go("/login") : setEmail("");
+            }}
+          />
+        </S.Wrapper>
+      </S.LoginWrapper>
     </S.Container>
   );
 };

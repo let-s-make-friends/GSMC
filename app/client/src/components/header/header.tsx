040aa052aa08c19f4c7d037ff27bc4975b0c7ff9
bae080311 <s24024@gsm.hs.kr>
2025-03-06 09:01:03 +0900

    ✨ Feat: navigation

--- app/client/src/components/header/header.tsx
+++ app/client/src/components/header/header.tsx
@@ -1,15 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import * as S from "./styles";
 
 const Header = () => {
+  const go = useNavigate();
   return (
     <S.Header>
-      <S.Title>GSM 인증제</S.Title>
+      <S.Title onClick={() => go("/main")}>GSM 인증제</S.Title>
       <S.Nav>
         <li>점수 계산 해보기</li>
         <li>예시 보기</li>
         <li>팀소개</li>
       </S.Nav>
-      <S.Profile>배경진</S.Profile>
+      <S.Profile onClick={() => go("/profile")}>배경진</S.Profile>
     </S.Header>
   );
 };
